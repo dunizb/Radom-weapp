04d3e22f73a4465def63c02f300d888667c821de
Dunizb <zb@dunizb.vip>
2020-07-30 15:24:18 +0800

    代码优化

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,52 +1,52 @@
 //index.js
 //获取应用实例
-import Dialog from '../../miniprogram_npm/@vant/weapp/dialog/dialog';
+import Dialog from "../../miniprogram_npm/@vant/weapp/dialog/dialog";
 const app = getApp();
-const SRORAGE_KEY = 'list';
+const SRORAGE_KEY = "list";
 Page({
   data: {
     range: [],
     index: 0,
     show: false,
-    value: '',
-    textarea: '',
+    value: "",
+    textarea: "",
     dataList: [],
-    dialogTitle: '新列表',
-    isCreate: true
+    dialogTitle: "新列表",
+    isCreate: true,
   },
 
   onLoad: function () {
     this.init();
-    const list = wx.getStorageSync('list');
-    this.data.range = list.map(item => item.key);
+    const list = wx.getStorageSync("list");
+    this.data.range = list.map((item) => item.key);
     this.resetFirst(list);
   },
 
   init() {
-    const list = wx.getStorageSync('list') || [];
-    if(list.length === 0) { // 第一次使用，初始化一条数据
+    const list = wx.getStorageSync("list") || [];
+    if (list.length === 0) { // 第一次使用，初始化一条数据
       wx.setStorageSync(SRORAGE_KEY, [
         {
-          key: '今天吃什么',
-          list: ['火锅', '沙县小吃', '牛肉面', '回锅肉']
-        }
-      ])
+          key: "今天吃什么",
+          list: ["火锅", "沙县小吃", "牛肉面", "回锅肉"],
+        },
+      ]);
     }
   },
 
   start() {
-    const {dataList} = this.data;
-    if(dataList.length <= 1) {
+    const { dataList } = this.data;
+    if (dataList.length <= 1) {
       wx.showToast({
-        title: '至少添加两个数据项',
-        icon: 'none'
-      })
+        title: "至少添加两个数据项",
+        icon: "none",
+      });
     } else {
       const index = Math.floor(Math.random() * dataList.length);
       Dialog.alert({
-        title: '结果',
+        title: "结果",
         message: dataList[index],
-        customStyle: 'customStyle'
+        customStyle: "customStyle",
       }).then(() => {
         // on close
       });
@@ -64,33 +64,33 @@ Page({
     const list = wx.getStorageSync(SRORAGE_KEY);
     const value = this.data.value;
     let index = this.data.index;
-    if(value === '') {
+    if (value === "") {
       wx.showToast({
-        title: '你什么都没输入',
-        icon: 'none'
-      })
+        title: "你什么都没输入",
+        icon: "none",
+      });
     } else {
-      if(this.data.isCreate) { // 创建新列表
+      if (this.data.isCreate) { // 创建新列表
         index = list.length;
-        this.data.range.push(this.data.value)
+        this.data.range.push(this.data.value);
         list.push({
           key: this.data.value,
-          list: []
-        })
+          list: [],
+        });
         this.setData({
           range: this.data.range,
-          textarea: '',
-          index
+          textarea: "",
+          index,
         });
-      } else {  // 编辑
-        if(this.data.range[index] === value) { // 值相同，没修改
+      } else { // 编辑
+        if (this.data.range[index] === value) { // 值相同，没修改
           return;
         }
         this.data.range[index] = value;
-        list[index]['key'] = value;  
+        list[index]["key"] = value;
         this.setData({
-          range: this.data.range
-        });  
+          range: this.data.range,
+        });
       }
     }
     wx.setStorageSync(SRORAGE_KEY, list);
@@ -98,11 +98,11 @@ Page({
 
   bindTextAreaInput(e) {
     const val = e.detail.value;
-    if(!val) return;
-    this.data.dataList = val.replace(/，/g, ',').split(',');
-  
+    if (!val) return;
+    this.data.dataList = val.replace(/，/g, ",").split(",");
+
     const list = wx.getStorageSync(SRORAGE_KEY);
-    list[this.data.index][SRORAGE_KEY] = this.data.dataList;
+    list[this.data.index]["list"] = this.data.dataList;
     wx.setStorageSync(SRORAGE_KEY, list);
   },
 
@@ -113,50 +113,50 @@ Page({
     this.setData({
       index: index,
       value: data.key,
-      textarea: data.list.join('，')
+      textarea: data.list.join("，"),
     });
   },
 
   add() {
-    this.setData({ 
+    this.setData({
       show: true,
-      value: '',
+      value: "",
       isCreate: true,
-      dialogTitle: '新列表'
-    })
+      dialogTitle: "新列表",
+    });
   },
 
   delete() {
-    const {index, range} = this.data;
-    if(range.length === 1) {
+    const { index, range } = this.data;
+    if (range.length === 1) {
       wx.showToast({
-        title: '无法删除，至少保留一个项目',
-        icon: 'none'
-      })
+        title: "无法删除，至少保留一个项目",
+        icon: "none",
+      });
       return;
-    } 
+    }
     wx.showModal({
-      title: '提示',
-      content: '确定删除吗？',
+      title: "提示",
+      content: "确定删除吗？",
       success: (confirm) => {
-        if(confirm) {
+        if (confirm) {
           this.data.range.splice(index, 1);
           const list = wx.getStorageSync(SRORAGE_KEY);
           this.resetFirst(list);
           list.splice(index, 1);
           wx.setStorageSync(SRORAGE_KEY, list);
         }
-      }
-    })
+      },
+    });
   },
 
   edit() {
     const value = this.data.range[this.data.index];
-    this.setData({ 
+    this.setData({
       show: true,
-      dialogTitle: '修改列表',
+      dialogTitle: "修改列表",
       isCreate: false,
-      value
+      value,
     });
   },
 
@@ -168,7 +168,7 @@ Page({
       index: 0,
       range: this.data.range,
       dataList: list[0][SRORAGE_KEY],
-      textarea: list[0][SRORAGE_KEY].join('，')
+      textarea: list[0][SRORAGE_KEY].join("，"),
     });
-  }
-})
+  },
+});
